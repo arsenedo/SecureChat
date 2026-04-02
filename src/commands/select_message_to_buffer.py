4d9d9cf0d3ceaaca7421bf21e73ce382ffd353f6
Arsenii <arsenii.nedorezov@gmail.com>
2026-04-02 18:31:16 +0200

    improve message selection method

--- src/commands/select_message_to_buffer.py
+++ src/commands/select_message_to_buffer.py
@@ -4,11 +4,23 @@ from encoder import Encoder
 
 class SelectMessageToBuffer(ICommand):
     def __init__(self, user_input: str):
-        self.message_id = int(user_input)
+        self.user_input = user_input if user_input != None else ""
 
     def execute(self, tcp_client: Client, encoder: Encoder):
-        payload = tcp_client.payload_history[self.message_id - 1]
-        message = payload.message.decode("utf-8")
+        params = self.user_input.split(" ")
+        if len(params) != 2:
+            print("Not enough parameters provided")
+            return
+        
+        message_id = int(params[0])
+        buffer = params[1]
 
-        encoder.set_plain_buffer(message)
-        encoder.set_encoded_buffer(message)
+        payload = tcp_client.payload_history[message_id - 1]
+
+        if buffer == "e":
+            encoder.set_encoded_buffer(payload.message)
+        elif buffer == "c":
+            encoder.set_plain_buffer(payload.message)
+        else:
+            print("Please specify a valid buffer")
+
